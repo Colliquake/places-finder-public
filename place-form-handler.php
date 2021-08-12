87485a1f317f292b3db94a10c7542b518ca0a56e
Colliquake <aroh002@ucr.edu>
2021-08-11 18:30:52 -0700

    api key

--- place-form-handler.php
+++ place-form-handler.php
@@ -2,7 +2,7 @@
     $location = $_POST["location"];
     echo $location;
 
-    $contents = file_get_contents('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.865857,-117.233742&radius=1500&key=AIzaSyCNPRaXXT7GmGaQpCvY2FpkqrP9dhKdjss');
+    $contents = file_get_contents('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.865857,-117.233742&radius=1500&key=???');
 
     echo $contents;
 ?>
