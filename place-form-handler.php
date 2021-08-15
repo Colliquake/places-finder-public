8d43c3462a6bcfc9f3ffa8daf29eb4ad805b081c
Colliquake <aroh002@ucr.edu>
2021-08-15 11:29:56 -0700

    get geocode

--- place-form-handler.php
+++ place-form-handler.php
@@ -1,8 +1,39 @@
 <?php
     $location = $_POST["location"];
-    echo $location;
+    $location = urlencode($location);
+    $loc_arr = get_geocode($location);
 
-    $contents = file_get_contents('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.865857,-117.233742&radius=1500&key=AIzaSyAISncWGWJBn3bSM0O8AxyW2hJjkmtbx6o');
+    function get_geocode($address){
+        $url = "https://maps.googleapis.com/maps/api/geocode/json?address=".$address."&key=AIzaSyAISncWGWJBn3bSM0O8AxyW2hJjkmtbx6o";
+        $content = file_get_contents($url);
 
-    echo $contents;
+        // decode the json response
+        $resp = json_decode($resp_json, true);
+        // response status will be 'OK', if able to geocode given address
+        if($resp['status']=='OK'){
+    	    //define empty array
+ 		    $data_arr = array(); 
+            // get the important data
+            $data_arr['latitude'] = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : '';
+            $data_arr['longitude'] = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : '';
+            $data_arr['formatted_address'] = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : '';
+        
+            // verify if data is exist
+            if(!empty($data_arr) && !empty($data_arr['latitude']) && !empty($data_arr['longitude'])){
+                return $data_arr;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
+    var_dump(loc_arr);
+
+    // $contents = file_get_contents('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.865857,-117.233742&radius=1500&key=AIzaSyAISncWGWJBn3bSM0O8AxyW2hJjkmtbx6o');
+
+    // echo $contents;
 ?>
