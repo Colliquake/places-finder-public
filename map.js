f2b151aacf77a73e0f031b703f173d496bcb3e11
Colliquake <aroh002@ucr.edu>
2021-08-19 16:35:10 -0700

    other approach

--- map.js
+++ map.js
@@ -1,11 +1,11 @@
-function initMap(){
-    // gMap = new google.maps.maps(document.getElementById('map'));
-    // gMap.setCenter(new google.maps.LatLng(39.8097343, -98.5556199));
-    // gMap.setZoom(3);
+let map;
 
-    var gMap = {lat: 39.8097343, lng: -98.5556199};
-    var map = new google.maps.map(document.getElementById('map'), {
-        zoom: 3,
-        center: gMap,
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {   //centers onto USA
+            lat: 39.8097343,
+            lng: -98.5556199
+        },
+        zoom: 3
     });
 }
