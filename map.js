593b172fbef2e9fae56656824096e7f0213482d2
Colliquake <aroh002@ucr.edu>
2021-08-19 14:46:00 -0700

    different initMap

--- map.js
+++ map.js
@@ -1,5 +1,11 @@
 function initMap(){
-    gMap = new google.maps.maps(document.getElementById('map'));
-    gMap.setCenter(new google.maps.LatLng(39.8097343, -98.5556199));
-    gMap.setZoom(3);
+    // gMap = new google.maps.maps(document.getElementById('map'));
+    // gMap.setCenter(new google.maps.LatLng(39.8097343, -98.5556199));
+    // gMap.setZoom(3);
+
+    const gMap = {lat: 39.8097343, lng: -98.5556199};
+    const map = new google.maps.map(document.getElementById('map'), {
+        zoom: 3,
+        center: gMap,
+    });
 }
